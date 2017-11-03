a1a02cdd52741dcc58203b33b2113cf79e05659f
gongxiaole <915923345@qq.com>
2017-11-03 15:20:36 +0800

    一级分类页渲染完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -4,7 +4,7 @@
 
 $(function () {
     var currentPage = 1;
-    var pageSize = 2;
+    var pageSize = 5;
     function render() {
         $.ajax({
             type:"get",
@@ -29,4 +29,44 @@ $(function () {
         })
     }
     render();
+    $(".btn_add").on("click", function () {
+        $("#addModal").modal("show");
+    });
+    var $form = $("#form");
+    $form.bootstrapValidator({
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields:{
+            categoryName:{
+                validators:{
+                    notEmpty:{
+                        message:"一级分类名称不能为空"
+                    }
+                }
+            }
+        }
+    });
+    $form.on("success.form.bv", function (e) {
+        e.preventDefault();//不让他跳转
+        // console.log("hehe");
+        $.ajax({
+            type:"post",
+            url:"/category/addTopCategory",
+            data:$form.serialize(),
+            success:function (data) {
+                if(data.success){
+                    $("#addModal").modal("hide");
+                    //渲染第一页
+                    currentPage = 1;
+                    render();
+                    //重置表单
+                    $form.data("bootstrapValidator").resetForm();
+                    $form[0].reset();
+                }
+            }
+        });
+    })
 });
